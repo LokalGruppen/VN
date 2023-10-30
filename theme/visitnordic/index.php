1d018dbbaf138283962f6094e2bca787608f9a73
LokalGruppen <daniel@lokal-gruppen.dk>
2023-10-30 15:31:40 +0100

    Update index.php

--- theme/visitnordic/index.php
+++ theme/visitnordic/index.php
@@ -3,7 +3,7 @@ use Joomla\CMS\Factory;
 
 defined('_JEXEC') or die;
 
-require_once __DIR__ . '/helper.php';  // Explicitly include helper.php
+require_once __DIR__ . '/includes/helper.php';  // Explicitly include helper.php
 include __DIR__ .'/includes/params.php';
 
 $helper = vnTemplateHelper::getInstance($this);  // Removed the namespace
